8d903bad29fb20db1e81882cab3df3749b86d1e9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 19:37:34 +0430

    init

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const img = document.querySelector('.user-img')
 const getElement = (selection) => {
     const element = document.querySelector(selection)
     if(element) return element
+    throw new Error('no element selected')
 }
