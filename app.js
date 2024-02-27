31142282afe7a2404de6924dc54d2923f5a3d49f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 08:13:21 +0430

    init

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const URL = 'https://randomuser.me/api/'
 
 const img = get('.user-img')
 const title = get('.user-title')
-const value = get('.user-img')
+const value = get('.user-value')
 const btn = get('.btn')
