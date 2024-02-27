5b226079e38a71c1639a7deeabb666a27b20da34
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 19:36:25 +0430

    init

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const URL = 'https://randomuser.me/api/'
 const img = document.querySelector('.user-img')
 
 const getElement = (selection) => {
-
+    const element = document.querySelector(selection)
 }
