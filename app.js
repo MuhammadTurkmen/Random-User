ef0472c0b0d979f54e35df8c418fa481e6184ef4
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 08:33:33 +0430

    init

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const getUser = async () => {
     // destructure
     const person = data.results[0]
     const {phone, email} = person
-    const {large} = person.picture
+    const {large: image} = person.picture
 }
 
 
