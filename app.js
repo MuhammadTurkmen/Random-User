810f0323b4594b31f466a55d70a71aabaa1685f6
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 08:12:11 +0430

    init

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ import get from "./utils/getElement"
 const URL = 'https://randomuser.me/api/' 
 
 const img = get('.user-img')
+const img = get('.user-img')
+const img = get('.user-img')
+const img = get('.user-img')
