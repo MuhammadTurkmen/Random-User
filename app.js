125351794b30e100852b0e0d0c3a6636de9475e9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 09:22:37 +0430

    init

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const displayUser = (person) => {
  
 
 const showUser = async () => {
-    // get user from api
+    // get user from api 
     const person = await getUser()
     displayUser(person)
     // display user
