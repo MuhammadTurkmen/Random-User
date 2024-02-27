1e3955e73e9d5775cf3d9b5f32822c4935694f85
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-27 08:26:22 +0430

    init

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const btns = [...document.querySelectorAll('.icon')]
 
 const getUser = async () => {
     const response = await fetch(URL)
+    const data = await response.json()
     console.log(response);
 }
 
