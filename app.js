9397b408dfb931ff1aa04fe6075a40300c0a8e6c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-28 09:35:07 +0430

    init

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const displayUser = (person) => {
     img.src = person.image
     value.textContent = person.name
     title.textContent = `My name is`
+    btns.forEach((btn) => {
+        btn.classList.remove('active')
+    })
+
     btns[0].classList.add('active')
     btns.forEach((btn) => {
         const label = btn.dataset.label
